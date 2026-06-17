8d6be903f14ad20cbf42f599d99070577d5dfcb5
Kajal805-M <mauryakajal444@gmail.com>
2026-06-17 11:35:52 +0530

    Update remove element solution

--- 3 Remove Element.py
+++ 3 Remove Element.py
@@ -1,8 +1,8 @@
 class Solution(object):
-    def removeElement(self, nums, val):
+    def removeElement(self, nums, value):
         self=0
         for i in range(len(nums)):
-            if nums[i] != val:
+            if nums[i] != value:
                 nums[self] = nums[i]
                 self += 1
         return self

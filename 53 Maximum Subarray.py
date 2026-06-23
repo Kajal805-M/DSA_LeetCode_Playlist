5703124eca2f8abdc971f753549001939a769023
Kajal805-M <mauryakajal444@gmail.com>
2026-06-23 12:19:47 +0530

    Maximum Subarray

---
+++ 53 Maximum Subarray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        max_so_far=nums[0]
+        max_ending_here=nums[0]
+        if not nums:
+            return 0
+
+        for i in range(1, len(nums)):
+            max_ending_here=max(nums[i], max_ending_here+nums[i])
+            max_so_far= max(max_so_far , max_ending_here)
+        return max_so_far
